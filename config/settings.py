cdfa5161256a85ee6746649793ddf55ac253d0e6
mdy <h20096@e.hiroshima-cu.ac.jp>
2025-03-16 12:19:23 +0900

    fix heroku settings file

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,8 @@ env = environ.Env(DEBUG=(bool, False))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+if os.path.join(BASE_DIR, ".env"):
+    environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
